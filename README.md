1dd9f22e240641e64a870a7d1a6efb2e7be349d0
B0rj4 <borjita89@yahoo.es>
2025-05-30 16:36:58 -0500

    fix some test and add some mock data bc the api is slow

--- README.md
+++ README.md
@@ -10,24 +10,19 @@ Pruebas, abrir cypress
 
 Para que funcionen las pruebas con usuarios hay que tener ejecutando el servidor:
 
-https://github.com/luispedraza/buscasofa-server 
+https://github.com/luispedraza/buscasofa-server
 
+# Recursos útiles
 
+Documentación de vite: https://vite.dev/guide/
+Documentación de cypress: https://www.cypress.io/
+cypress-cucumber-preprocessor: https://github.com/badeball/cypress-cucumber-preprocessor?tab=readme-ov-file
 
-
-Recursos útiles
-===============
-
-Documentación de vite: https://vite.dev/guide/ 
-Documentación de cypress: https://www.cypress.io/ 
-cypress-cucumber-preprocessor: https://github.com/badeball/cypress-cucumber-preprocessor?tab=readme-ov-file 
-
-Otros: 
+Otros:
 
 Tutorial Cypress + React: https://www.youtube.com/watch?v=6BkcHAEWeTU
-Explica cómo instalar cypress en una aplicación react y escribir un test para probar una aplicación sencilla 
+Explica cómo instalar cypress en una aplicación react y escribir un test para probar una aplicación sencilla
 
-Tutorial cypress + cucumber --> react https://www.youtube.com/playlist?list=PLyWVU-yS4Fb7UcKP8ElLsZklMO86vLdHQ 
+Tutorial cypress + cucumber --> react https://www.youtube.com/playlist?list=PLyWVU-yS4Fb7UcKP8ElLsZklMO86vLdHQ
 
-Mocking en cypress https://kailash-pathak.medium.com/mocking-api-response-in-cypress-a73dea514cfd 
-
+Mocking en cypress https://kailash-pathak.medium.com/mocking-api-response-in-cypress-a73dea514cfd
--- cypress/components/Header.cy.jsx
+++ cypress/components/Header.cy.jsx
@@ -1,10 +1,78 @@
 /// <reference types="Cypress" />
 // import React from 'react'
-import Header from '../../src/components/Header'
+import { MemoryRouter } from 'react-router-dom';
+import Header from '../../src/components/Header';
 
 describe('<Header />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<Header />)
-  })
-})
+  beforeEach(() => {
+    // Mock para rutas (evita errores de router)
+    cy.mount(
+      <MemoryRouter>
+        <Header user={null} />
+      </MemoryRouter>
+    );
+  });
+
+  context('Visual Elements', () => {
+    it('should display logo', () => {
+      cy.get('img[alt="Logo"]')
+        .should('be.visible')
+        .and('have.attr', 'src')
+        .then((src) => {
+          expect(src).to.include('logo.png');
+        });
+    });
+
+    it('should show navigation links', () => {
+      const links = ['Buscador', 'Mapa', 'Acerca de nosotros'];
+      links.forEach((text) => {
+        cy.contains('a', text).should('be.visible');
+      });
+    });
+  });
+
+  context('Guest User State', () => {
+    it('should show login/register links when no user', () => {
+      cy.contains('a', 'Login').should('exist');
+      cy.contains('a', 'Registro').should('exist');
+      cy.contains('button', 'Cerrar sesión').should('not.exist');
+    });
+
+    // it('should navigate to login page', () => {
+    //   cy.contains('a', 'Login').click();
+    //   cy.location('pathname').should('eq', '/login');
+    // });
+  });
+
+  context('Logged-in User State', () => {
+    beforeEach(() => {
+      cy.mount(
+        <MemoryRouter>
+          <Header user="testuser" />
+        </MemoryRouter>
+      );
+    });
+
+    it('should show welcome message and logout button', () => {
+      cy.contains('Bienvenido, testuser').should('be.visible');
+      cy.contains('button', 'Cerrar sesión').should('exist');
+      cy.contains('a', 'Login').should('not.exist');
+    });
+
+    // it('should call logout handler', () => {
+    //   const spy = cy.spy().as('logoutSpy');
+    //   cy.mount(
+    //     <MemoryRouter>
+    //       <Header user="testuser" handleLogout={spy} />
+    //     </MemoryRouter>
+    //   );
+    //   cy.contains('button', 'Cerrar sesión').click();
+    //   cy.get('@logoutSpy').should('have.been.calledOnce');
+    // });
+
+    // it('should link to user profile', () => {
+    //   cy.contains('a', 'testuser').click();
+    //   cy.location('pathname').should('eq', '/perfil');
+    // });
+  });
+});
