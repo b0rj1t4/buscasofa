ca191301ee53206285c212cba6df560d43ba0447
B0rj4 <borjita89@yahoo.es>
2025-06-02 21:26:49 -0500

    remove not needed mock

--- cypress/e2e/emergency.cy.js
+++ cypress/e2e/emergency.cy.js
@@ -6,10 +6,57 @@ describe('Visualización de la página de Emergencia', () => {
     cy.intercept('GET', '**/EstacionesTerrestres/**', {
       fixture: '../fuel_prices.json', // Mock de la respuesta de la API
     }).as('getFuelPrices');
-    cy.visit('/emergency');
+
+    cy.visit('/emergency', {
+      onBeforeLoad(win) {
+        cy.stub(win.navigator.geolocation, 'getCurrentPosition').callsFake(
+          (cb) => {
+            cb({
+              coords: {
+                latitude: 40.4168,
+                longitude: -3.7038,
+              },
+            });
+          }
+        );
+      },
+    });
   });
 
-  it('El usuario ve el título de la página de Emergencia', () => {
-    cy.get('h1').should('exist').contains('Emergencia');
+  it('muestra las gasolineras más cercanas', () => {
+    cy.contains('h2', 'Gasolineras más cercanas').should('be.visible');
+
+    cy.get('.station-card').should('have.length', 10);
+
+    cy.get('.station-card')
+      .first()
+      .within(() => {
+        cy.contains(/Distancia:/);
+        cy.contains(/Dirección:/);
+        cy.contains(/Horario:/);
+        cy.contains(/Gasóleo A:/);
+        cy.contains(/Gasolina 95 E5:/);
+      });
+  });
+
+  describe('cuando no hay geolocalizacion', () => {
+    beforeEach(() => {
+      cy.intercept('GET', '**/EstacionesTerrestres/**', {
+        body: [],
+      });
+      cy.visit('/emergency');
+    });
+
+    it('muestra un mensaje de error no localizacion', () => {
+      cy.get('.error')
+        .should('exist')
+        .and('contain', 'Error al obtener ubicación');
+    });
+
+    it('muestra input para introducir ubicación manualmente', () => {
+      cy.get('h3').should('exist').and('contain', 'código postal');
+      cy.get('input').should('exist');
+      cy.get('button').should('exist').and('contain', 'Buscar');
+    });
   });
 });
